b3e0284a7c451ce4f3ea6619fe9572f4854ad166
franBlopez <francisco.lopez@alten.es>
2018-05-08 19:15:57 +0200

    add first services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Categories } from './categories/categories.interface';
+import { CategoriesService } from './categories/categories.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    private categories: Categories[] = [];
+
+    constructor(private categoriesService: CategoriesService) {}
+
+    ngOnInit() {
+        this.categoriesService.getCategories()
+            .subscribe(categories => {
+                console.log(categories);
+                this.categories = categories;
+            });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        environment.production ?
+            [] : HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false })
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
